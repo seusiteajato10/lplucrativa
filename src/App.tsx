8176d57ebc427f04bcb9a98390faa1952d687d0a
[dyad] <seusiteajato10@gmail.com>
2025-12-26 15:39:12 -0300

    [dyad] Corrigindo redirecionamento de autenticação - wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ProjectsProvider } from "@/contexts/ProjectsContext";
@@ -38,7 +38,7 @@ const App = () => {
         <QueryClientProvider client={queryClient}>
           <AuthProvider>
             <ProjectsProvider>
-              <BrowserRouter>
+              <HashRouter>
                 <Routes>
                   <Route path="/" element={<Index />} />
                   <Route path="/login" element={<Login />} />
@@ -67,7 +67,7 @@ const App = () => {
                   {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                   <Route path="*" element={<NotFound />} />
                 </Routes>
-              </BrowserRouter>
+              </HashRouter>
             </ProjectsProvider>
           </AuthProvider>
         </QueryClientProvider>
